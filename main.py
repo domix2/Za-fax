99696b46d3a9257739c20f6a2b0e9ad48501aa47
Domagoj <domagoj.speljko@gmail.com>
2023-03-14 16:16:45 +0100

    Prvi labosi i pokušaj koda

---
+++ main.py
@@ -0,0 +1,9 @@
+print('Dobrodošli','!'*3)
+
+print('Unesite vaše ime : ' , 'Unesite vaše prezime :' , 'Unesite vaš JMBAG :' , 'Unesite svoje godine:')
+ime = input()
+prezime = input()
+JMBAG = input()
+godine = input()
+
+print("Korisnik:" ,ime,"",prezime,"",JMBAG,"",godine,"",'!')
